a6a8d6c4e54a07f39d34742afca097ab11f47d46
Emrecan Balgün <deathlygrave@gmail.com>
2022-03-16 19:48:54 +0300

    refactor index.js

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+
 const { users, posts, comments } = require('./data');
 
 const typeDefs = gql`
+    type User{
+        id: ID!
+        fullName: String!
+    }
 
+    type Post{
+        id: ID!
+        title: String!
+        user_id: ID!
+    }
+
+    type Comments{
+        id: ID!
+        text: String!
+        post_id: ID!
+    }
+
+    type Query{
+        users: [User!]!
+        posts: [Post!]!
+        comments: [Comments!]!
+    }
 `
 
 const resolvers = {
     Query: {
-
+        users: () => users,
+        posts: () => posts,
+        comments: () => comments,
     },
 }
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({            
+    typeDefs,
+    resolvers,
+    plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground({})
+    ]
+});
 
 server.listen().then(( { url } ) => {
     console.log(`Server ready at ${url}`) 
