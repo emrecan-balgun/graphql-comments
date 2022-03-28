e6cf045a9cfacacd6344f33ba28bb3a3fceab244
Emrecan Balgün <deathlygrave@gmail.com>
2022-03-28 13:22:02 +0300

    update comment js

--- src/graphql/resolvers/Comment.js
+++ src/graphql/resolvers/Comment.js
@@ -1,5 +1,5 @@
 const Comment = {
-    user: (parent, _, { db }) => db. users.find((user) => user.id === parent.user_id),
+    user: (parent, _, { db }) => db.users.find((user) => user.id === parent.user_id),
 
     post: (parent, _, { db }) => db.posts.find((post) => post.id === parent.post_id)
 }
