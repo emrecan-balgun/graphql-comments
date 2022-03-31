e6a31a063b4db5b254e1c2c9c244ff5406c66e76
Emrecan Balgün <deathlygrave@gmail.com>
2022-03-31 21:58:41 +0300

    add apollo.js and update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {
-  ApolloClient,
-  InMemoryCache,
   ApolloProvider,
 } from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache()
-});
+import client from './apollo';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
