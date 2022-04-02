f14e4f7fd86c0dcc437ebc5bc56b2c0175d90986
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-02 19:27:51 +0300

    update header menu and css defines

--- client/src/components/HeaderMenu.js
+++ client/src/components/HeaderMenu.js
@@ -1,13 +1,21 @@
 import { Menu } from 'antd';
+import styles from './App/styles.module.css';
+
+import { Link, useLocation } from 'react-router-dom';
 
 function HeaderMenu() {
-  return (
-    <Menu mode="horizontal">
-        <Menu.Item key="mail">
-            Home
+    const location = useLocation();
+
+    return (
+    <Menu mode="horizontal" selectedKeys={location.pathname} className={styles.headerMenu}>
+        <Menu.Item key="/" className={styles.menuItem}>
+            <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item key="/new" className={styles.menuItem}>
+            <Link to="/new">New</Link>
         </Menu.Item>
     </Menu>
-  )
+    )
 }
 
 export default HeaderMenu
