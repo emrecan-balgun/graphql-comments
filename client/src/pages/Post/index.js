7496b695d632945e6af380ffb055feccd0d73764
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-03 02:32:38 +0300

    update post and add queries to post

--- client/src/pages/Post/index.js
+++ client/src/pages/Post/index.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GET_POST } from './queries';
+import Loading from 'components/Loading';
+import { Typography } from 'antd';
+import { Image } from 'antd';
+
+const { Title } = Typography;
 
 function Post() {
+    const { id } = useParams();
+
+    const { loading, error, data } = useQuery(GET_POST, {
+        variables: {
+            id,
+        },
+    });
+    
+    if (loading) {
+        return <Loading />
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>
+    }
+
+    const { post } = data;
+
   return (
-    <h1>Post</h1>
+    <div>
+         <Title level={3}>{post.title}</Title>
+         <Image src={post.cover} />
+         <div>
+            {post.description}
+         </div>
+    </div>
   )
 }
 
