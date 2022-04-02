fd55e98e44796ec1486d605703b2bb754247a83d
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-03 01:48:52 +0300

    add loading page, update home

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,57 +2,26 @@ import { List, Avatar } from 'antd';
 
 import { gql, useQuery } from '@apollo/client';
 
+import Loading from 'components/Loading'
+
 const GET_POSTS = gql`
-  query getAllPosts {
-  posts{
-    id
-    title
-    description
-    user{
-      profile_photo
+    query getAllPosts {
+    posts{
+      id
+      title
+      description
+      user{
+        profile_photo
+      }
     }
   }
-}
 `;
-
-const list = [
-  {
-    gender: 'female',
-    name: {
-      title: "Miss",
-      first: "آوینا",
-      last: "پارسا"
-    },
-    email: "awyn.prs@example.com",
-    picture: {
-      large: "https://randomuser.me/api/portraits/women/17.jpg",
-      medium: "https://randomuser.me/api/portraits/med/women/17.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/women/17.jpg",
-    },
-    nat: "IR",
-  },
-  {
-    gender: 'female',
-    name: {
-      title: "Miss",
-      first: "آوینا",
-      last: "پارسا"
-    },
-    email: "awyn.prs@example.com",
-    picture: {
-      large: "https://randomuser.me/api/portraits/women/17.jpg",
-      medium: "https://randomuser.me/api/portraits/med/women/17.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/women/17.jpg",
-    },
-    nat: "IR",
-    },
-]
 
 function Home() {
   const { loading, error, data } = useQuery(GET_POSTS);
 
   if (loading) {
-    return <div>Loading...</div>
+    return <Loading />
   }
 
   if (error) {
@@ -61,13 +30,13 @@ function Home() {
 
   return (
     <List
-        dataSource={list}
+        dataSource={data.posts}
         renderItem={item => (
             <List.Item key={item.id}>
             <List.Item.Meta
-                avatar={<Avatar src={item.picture.large} />}
-                title={<a href="https://ant.design">{item.name.last}</a>}
-                description={item.email}
+                avatar={<Avatar src={item.user.profile_photo} />}
+                title={<a href="https://ant.design">{item.title}</a>}
+                description={item.description}
             />
             </List.Item>
         )}
