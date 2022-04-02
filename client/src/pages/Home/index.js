70ca44b663ab0e41a77532e4a4dac9e6aa4395f0
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-03 01:38:01 +0300

    add use query from apollo client

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,21 @@
 import { List, Avatar } from 'antd';
 
-const data = [
+import { gql, useQuery } from '@apollo/client';
+
+const GET_POSTS = gql`
+  query getAllPosts {
+  posts{
+    id
+    title
+    description
+    user{
+      profile_photo
+    }
+  }
+}
+`;
+
+const list = [
   {
     gender: 'female',
     name: {
@@ -34,9 +49,19 @@ const data = [
 ]
 
 function Home() {
+  const { loading, error, data } = useQuery(GET_POSTS);
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
+
   return (
     <List
-        dataSource={data}
+        dataSource={list}
         renderItem={item => (
             <List.Item key={item.id}>
             <List.Item.Meta
