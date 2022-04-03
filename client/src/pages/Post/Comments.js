ab3112787845690280381a3e6fbc40c807fc6eaf
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-04 00:56:46 +0300

    add comments list

--- client/src/pages/Post/Comments.js
+++ client/src/pages/Post/Comments.js
@@ -1,28 +1,67 @@
 import { Divider, Button } from 'antd';
+import { Comment, List } from 'antd';
 import styles from './styles.module.css';
-import Loading from 'components/Loading';
 import { useLazyQuery } from '@apollo/client'
 import { GET_POST_COMMENTS } from './queries';
+
+const comments = [
+    {
+      actions: [<span key="comment-list-reply-to-0">Reply to</span>],
+      author: 'Han Solo',
+      avatar: 'https://joeschmoe.io/api/v1/random',
+      content: (
+        <p>
+          We supply a series of design principles, practical patterns and high quality design
+          resources (Sketch and Axure), to help people create their product prototypes beautifully and
+          efficiently.
+        </p>
+      ),
+    },
+    {
+      actions: [<span key="comment-list-reply-to-0">Reply to</span>],
+      author: 'Han Solo',
+      avatar: 'https://joeschmoe.io/api/v1/random',
+      content: (
+        <p>
+          We supply a series of design principles, practical patterns and high quality design
+          resources (Sketch and Axure), to help people create their product prototypes beautifully and
+          efficiently.
+        </p>
+      ),
+    },
+  ];
 
 function Comments({ post_id }) {
     const [loadComments, { loading, data }] = useLazyQuery(GET_POST_COMMENTS, {
         variables: { id: post_id },
     });
 
-    if (loading) {
-        return <Loading />
-    }
-
   return (
       <>
         <Divider>Comments</Divider>
         <div className={styles.showCommentsBtnContainer}>
-            <Button onClick={ () => loadComments() }>Show Comments</Button>
-
-            <div>
-                YORUMLAR BURADA GORUNECEK
-            </div>
+            <Button loading={loading} onClick={ () => loadComments() }>Show Comments</Button>
         </div>
+            {
+                !loading && data &&
+                // <div>YORUMLAR</div>
+                <List
+                    className="comment-list"
+                    header={`${data.length} replies`}
+                    itemLayout="horizontal"
+                    dataSource={comments}
+                    renderItem={item => (
+                    <li>
+                        <Comment
+                        actions={item.actions}
+                        author={item.author}
+                        avatar={item.avatar}
+                        content={item.content}
+                        />
+                    </li>
+                    )}
+                />
+            }
       </>
     
   )
