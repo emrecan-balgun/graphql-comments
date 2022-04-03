a5810f691dde5217eb79a25693f1664a8d35fd06
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-04 01:06:58 +0300

    update btn visible and show comments

--- client/src/pages/Post/Comments.js
+++ client/src/pages/Post/Comments.js
@@ -3,24 +3,35 @@ import { Comment, List } from 'antd';
 import styles from './styles.module.css';
 import { useLazyQuery } from '@apollo/client'
 import { GET_POST_COMMENTS } from './queries';
+import { useEffect, useState } from 'react';
 
 function Comments({ post_id }) {
+    const [btnIsVisible, setBtnIsVisible] = useState(true);
+
     const [loadComments, { loading, data }] = useLazyQuery(GET_POST_COMMENTS, {
         variables: { id: post_id },
     });
+
+    useEffect(() => {
+        if(!loading && data) {
+            setBtnIsVisible(false);
+        }
+    }, [loading, data]);
 
   return (
       <>
         <Divider>Comments</Divider>
         <div className={styles.showCommentsBtnContainer}>
-            <Button loading={loading} onClick={ () => loadComments() }>Show Comments</Button>
+        {
+            btnIsVisible && <Button onClick={() => loadComments()}>Show Comments</Button>
+        }
         </div>
             {
                 !loading && data &&
                 // <div>YORUMLAR</div>
                 <List
                     className="comment-list"
-                    header={`${data.length} replies`}
+                    // header={`${data.length} replies`}
                     itemLayout="horizontal"
                     dataSource={data.post.comments}
                     renderItem={item => (
