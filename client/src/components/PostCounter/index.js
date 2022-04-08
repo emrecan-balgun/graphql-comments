cfd90476939159dbae6db5880ba738afce829267
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-08 16:50:20 +0300

    add post counter badge

--- client/src/components/PostCounter/index.js
+++ client/src/components/PostCounter/index.js
@@ -1,10 +1,14 @@
 import styles from './styles.module.css'
 import { Badge, Avatar } from 'antd';
+import { useSubscription } from '@apollo/client';
+import { POST_COUNT_SUBSCRIPTION } from './queries';
 
 function PostCounter() {
+    const { loading, data } = useSubscription(POST_COUNT_SUBSCRIPTION);
+
   return (
     <div className={styles.container}>
-        <Badge count={4} size="small">
+        <Badge count={loading ? "?" : data.postCount} size="small">
           <Avatar shape="square" size="medium">
             <span className={styles.counterTitle}>Posts</span>
           </Avatar>
