fa743e60918404dd509d76188abb16d28b0c4966
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-12 17:45:30 +0300

    update design new comment form

--- client/src/pages/Post/Comments/NewCommentForm.js
+++ client/src/pages/Post/Comments/NewCommentForm.js
@@ -1,5 +1,5 @@
 import styles from './styles.module.css';
-import { Form, Input, Button, Select } from 'antd';
+import { Form, Input, Button, Select, Row, Col } from 'antd';
 import { useQuery } from '@apollo/client';
 import { GET_USERS } from './queries.js';
 
@@ -18,40 +18,39 @@ function NewCommentForm() {
       onFinish={handleSubmit}
       autoComplete="off"
     >
-      <Form.Item
-        name="text"
-        rules={[{ required: true, message: 'Please enter a message!' }]}
-      >
-        <Input size="medium" placeholder="Text"/>
-      </Form.Item>
-
-      <Form.Item
-        name="user_id"
-        rules={[{ required: true, message: 'Please select user!' }]}
-      >
-        <Select 
-          disabled={get_users_loading} 
-          loading={get_users_loading} 
-          placeholder="Select a user" 
-          size="medium"
-        >
-          {
-            users_data && users_data.users.map((item) => 
-            <Option 
-              key={item.id} 
-              value={item.id}>
-                {item.fullName}
-            </Option>
-            )
-          }
-        </Select>
-      </Form.Item>
-
-      <Form.Item className={styles.buttons}>
-        <Button size="medium" type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
+        <Row gutter={24}>
+            <Col span={10}>
+                <Form.Item
+                    name="user_id"
+                    rules={[{ required: true, message: 'Please select user!' }]}
+                    >
+                    <Select 
+                    disabled={get_users_loading} 
+                    loading={get_users_loading} 
+                    placeholder="Select a user" 
+                    size="medium"
+                    >
+                    {
+                        users_data && users_data.users.map((item) => 
+                        <Option 
+                        key={item.id} 
+                        value={item.id}>
+                            {item.fullName}
+                        </Option>
+                        )
+                    }
+                    </Select>
+                </Form.Item>
+            </Col>
+            <Col span={14}>
+                <Form.Item
+                    name="text"
+                    rules={[{ required: true, message: 'Please enter a message!' }]}
+                    >
+                    <Input size="medium" placeholder="Text"/>
+                </Form.Item>
+            </Col>
+        </Row>    
     </Form>
   )
 }
