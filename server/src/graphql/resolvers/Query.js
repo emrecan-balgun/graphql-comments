83f17b46f087671a5a1f2755fb621b0c8ef84ca9
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-14 02:14:38 +0300

    refactor user to query.js

--- server/src/graphql/resolvers/Query.js
+++ server/src/graphql/resolvers/Query.js
@@ -1,8 +1,14 @@
 export const Query = 
 {
     // User
-    users: (_, __, { db }) => db.users,
-    user: (_, args, { db }) => db.users.find((user) => user.id === args.id),
+    users: async (_, __, { _db }) => {
+        const users = await _db.User.find();
+        return users;
+    },
+    user: async (_, args, { _db }) => {
+        const user = await _db.User.findById(args.id);
+        return user;
+    },
 
     // Post
     posts: (_, __, { db }) => db.posts,
