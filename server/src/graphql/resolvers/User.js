dbfcddf701bd4ee234956844f31f34a790dcc011
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-14 02:48:27 +0300

    refactor user.js

--- server/src/graphql/resolvers/User.js
+++ server/src/graphql/resolvers/User.js
@@ -1,5 +1,8 @@
 export const User = {
-    posts: (parent, _, { db }) => db.posts.filter((post) => post.user_id === parent.id),
+    posts: async (parent, _, { _db }) => {
+        const posts = await _db.Post.find({ user: parent.id });
+        return posts;
+    },
 
     comments: (parent, _, { db }) => db.comments.filter((comment) => comment.user_id === parent.id)
 }
